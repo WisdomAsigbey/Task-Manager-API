bf4a21efd994c805295d22a459f3ebfde3c3f5d7
Wisdom Sackey Asigbey <wisdomasigbey55@gmail.com>
2025-09-02 21:16:12 +0300

    set middleware for static files

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ const tasks = require("./routes/task");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
